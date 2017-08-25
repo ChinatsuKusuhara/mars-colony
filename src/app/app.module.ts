93c2c0952d19abfcba78cf3cb3becdc455cdc5fb
Chinatsu <chinatsu.k@hotmail.com>
2017-08-25 08:40:41 -0700

    added src app back

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+import { EncountersComponent } from './components/encounters/encounters.component';
+import { ReportComponent } from './components/report/report.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+
+import { appRoutes } from './app.routes';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EncountersComponent,
+    ReportComponent,
+    WelcomeComponent,
+    RegisterComponent,
+    NotfoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    ReactiveFormsModule,
+    HttpModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
