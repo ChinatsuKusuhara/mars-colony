3e4e03f0dc632a8303db5261a9040b3bc151f66e
Chinatsu <chinatsu.k@hotmail.com>
2017-08-25 16:46:40 -0700

    get report working on report.ts

--- src/app/components/report/report.component.ts
+++ src/app/components/report/report.component.ts
@@ -1,48 +1,49 @@
-// import { Component, OnInit } from '@angular/core';
-// import { AlienService } from '../../services/alien';
+import { Component, OnInit } from '@angular/core';
+import { AlienService } from '../../services/alien';
 
-// import {
-//   FormControl,
-//   FormGroup,
-//   Validators,
-//   ValidatorFn
-// } from '@angular/forms';
+import { NewReport, Report } from '../../models/report';
+import { Alien } from '../../models/alien';
+
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  ValidatorFn
+} from '@angular/forms';
 
 
-// @Component({
-//   selector: 'app-report',
-//   templateUrl: './report.component.html',
-//   styleUrls: ['./report.component.scss'],
-//   providers: [
-//     AlienService
-//   ]
-// })
-// export class ReportComponent implements OnInit {
+@Component({
+  selector: 'app-report',
+  templateUrl: './report.component.html',
+  styleUrls: ['./report.component.scss'],
+  providers: [
+    AlienService
+  ]
+})
+export class ReportComponent implements OnInit {
 
-//   public alien: Alien[];\
+  alien: Alien[];
 
-//   registerForm = new FormGroup({
-//     name: new FormControl('', [
-//       Validators.required,
-//       Validators.maxLength(1000),
-//       Validators.minLength(1)
-//     ]),
-//     alien_id: new FormControl('none', [Validators.required])
-//   })
+  registerForm = new FormGroup({ 
+    atype: new FormControl('', [Validators.required]),
+    action: new FormControl('', [Validators.required]),
+  });
 
-//   constructor(
-//     private alienService: AlienService
-//   ) {}
+  constructor(
+    private alienService: AlienService
+  ) {}
 
-//   async ngOnInit() {
-//     const aliens = await this.alienService.getAliens();
-//     console.log(aliens);
-//   }
+  async ngOnInit() {
+    const aliens = await this.alienService.getAliens();
+    console.log(aliens);
 
-//   async registerAlien() {
-//     const newAlien: NewAlien = {
-//       name: this.
-//     }
-//   }
-
-// }
+    this.alien = aliens;
+  }
+  // async registerReport(){
+  //   const newReport: NewReport = {
+  //     date: '2011-11-11',
+  //     colonist_id: '3',  
+  //   }
+  // }
+  
+}
